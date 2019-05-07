ebca9eab3ea5047523c00f5dc5c3ee6bf56faa79
galvanize2 <galvanize2@fvfx1hhgj1wl.ci.org>
2019-05-07 16:05:43 -0600

    mouse drag draws

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ for (let i=0; i<50; i++) {
     }
 }
 
-//empty row
+//selected color row
 let rowDiv = document.createElement('div')
 rowDiv.className = "row"
-//rowDiv.style.height = '20px'
 mainDiv.appendChild(rowDiv)
+
+//selected color
 let textBox = document.createElement('textarea')
 rowDiv.appendChild(textBox)
 textBox.style.border = "2px black solid"
-//textBox.innerText = "No color selected"
 textBox.style.backgroundColor = 'black'
 textBox.style.marginTop = "2px"
 textBox.style.marginBottom = "2px"
+
 
 function paletteColorSelected (evt) {
     textBox.style.backgroundColor = evt.target.style.backgroundColor
@@ -31,6 +32,24 @@ function paletteColorSelected (evt) {
 function pixlBoxClick ( evt ) {
     evt.target.style.backgroundColor = textBox.style.backgroundColor
 }
+
+
+
+let isMouseButtonDown = false
+
+function pixlBoxMouseEnter ( evt ) {
+    if (!isMouseButtonDown) return
+    evt.target.style.backgroundColor = textBox.style.backgroundColor
+}
+
+function mouseButtonDown () {
+    isMouseButtonDown = true
+}
+
+function mouseButtonUp () {
+    isMouseButtonDown = false
+}
+
 //let paletteTag = document.querySelector(".palette")
 let paletteTag = document.createElement('div')
 paletteTag.className = "row"
@@ -63,8 +82,10 @@ for (let i=0; i<colors.length; i++) {
 let pxlBoxes = document.querySelectorAll('.pixl-box')
 for (let box of pxlBoxes) {
     box.addEventListener('click', pixlBoxClick)
+    box.addEventListener('mouseenter', pixlBoxMouseEnter)
 }
 
 
-
+mainDiv.addEventListener('mousedown', mouseButtonDown)
+mainDiv.addEventListener('mouseup', mouseButtonUp)
 
